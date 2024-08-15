86351fa7f4b9160d5fb9f600cb13ca4a7ec300ac
learning310 <1147781396@qq.com>
2024-08-15 14:09:29 +0800

    add semaphore testcase for 3.16 kernel

---
+++ semaphore-3.16/semaphore_test.c
@@ -0,0 +1,156 @@
+#include <linux/atomic.h>
+#include <linux/delay.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/rwsem.h>
+#include <linux/sched.h>
+#include <linux/seq_file.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
+
+static DECLARE_RWSEM(rwsem);
+#define PROC_FILE "rwsem_test"
+
+enum rwsem_waiter_type {
+	RWSEM_WAITING_FOR_WRITE,
+	RWSEM_WAITING_FOR_READ,
+};
+
+struct rwsem_waiter {
+	struct list_head list;
+	struct task_struct *task;
+	enum rwsem_waiter_type type;
+};
+
+static int rwsem_show(struct seq_file *m, void *v)
+{
+	struct rwsem_waiter *waiter;
+	seq_printf(m, "semaphore.count: 0x%016lx\n",
+		   atomic_long_read(&rwsem.count));
+
+	if (!list_empty(&rwsem.wait_list)) {
+		// The first waiter
+		waiter = list_entry(rwsem.wait_list.next, struct rwsem_waiter,
+				    list);
+		seq_printf(m, "\twait list: comm = %s, type = %s\n",
+			   waiter->task->comm,
+			   waiter->type == 0 ? "RWSEM_WAITING_FOR_WRITE" :
+					       "RWSEM_WAITING_FOR_READ");
+
+		// The rest waiter if have
+		list_for_each_entry(waiter, rwsem.wait_list.next, list) {
+			seq_printf(m, "\twait list: comm = %s, type = %s\n",
+				   waiter->task->comm,
+				   waiter->type == 0 ?
+					   "RWSEM_WAITING_FOR_WRITE" :
+					   "RWSEM_WAITING_FOR_READ");
+		}
+	}
+	return 0;
+}
+
+int downread(void *data)
+{
+	down_read(&rwsem);
+	do_exit(0);
+}
+
+int upread(void *data)
+{
+	up_read(&rwsem);
+	do_exit(0);
+}
+
+int downwrite(void *data)
+{
+	down_write(&rwsem);
+	do_exit(0);
+}
+
+int upwrite(void *data)
+{
+	up_write(&rwsem);
+	do_exit(0);
+}
+
+static ssize_t rwsem_write(struct file *file, const char __user *buf,
+			   size_t count, loff_t *offs)
+{
+	struct task_struct *task = NULL;
+	char cmd[40];
+
+	if (*offs)
+		return -EINVAL;
+
+	if (copy_from_user(cmd, buf, count))
+		return -EFAULT;
+
+	cmd[count - 1] = '\0';
+
+	if (strncmp(cmd, "down_read", 9) == 0) {
+		task = kthread_create(downread, cmd + 10, "%s", cmd + 10);
+		if (IS_ERR(task)) {
+			return -EFAULT;
+		}
+		wake_up_process(task);
+	} else if (strncmp(cmd, "up_read", 7) == 0) {
+		task = kthread_create(upread, cmd + 8, "%s", cmd + 8);
+		if (IS_ERR(task)) {
+			return -EFAULT;
+		}
+		wake_up_process(task);
+	} else if (strncmp(cmd, "down_write", 10) == 0) {
+		task = kthread_create(downwrite, cmd + 11, "%s", cmd + 11);
+		if (IS_ERR(task)) {
+			return -EFAULT;
+		}
+		wake_up_process(task);
+	} else if (strncmp(cmd, "up_write", 8) == 0) {
+		task = kthread_create(upwrite, cmd + 9, "%s", cmd + 9);
+		if (IS_ERR(task)) {
+			return -EFAULT;
+		}
+		wake_up_process(task);
+	}
+
+	return count;
+}
+
+static int rwsem_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, rwsem_show, NULL);
+}
+
+static const struct file_operations rwsem_fops = {
+	.open = rwsem_open,
+	.read = seq_read,
+	.write = rwsem_write,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int __init semaphore_test_init(void)
+{
+	struct proc_dir_entry *pe;
+
+	pe = proc_create(PROC_FILE, 0644, NULL, &rwsem_fops);
+	if (!pe)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static void __exit semaphore_test_exit(void)
+{
+	printk(KERN_ALERT "[Goodbye] semaphore_test\n");
+	remove_proc_entry(PROC_FILE, NULL);
+}
+
+module_init(semaphore_test_init);
+module_exit(semaphore_test_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("datawolf");
